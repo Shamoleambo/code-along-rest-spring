3fc45789b1e753f1fb4d62ec9fabe2a7c7b0e157
Tiago Gomes <tidgomes@gmail.com>
2025-02-11 16:00:57 -0300

    create endpoint to retrieve single student by id

--- src/main/java/com/tidz/rest/rest/StudentRestController.java
+++ src/main/java/com/tidz/rest/rest/StudentRestController.java
@@ -3,6 +3,7 @@ package com.tidz.rest.rest;
 import com.tidz.rest.entity.Student;
 import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +27,10 @@ public class StudentRestController {
     @GetMapping("/students")
     public List<Student> getStudents() {
         return this.students;
+    }
+
+    @GetMapping("/students/{id}")
+    public Student getStudent(@PathVariable int id) {
+        return this.students.get(id);
     }
 }
