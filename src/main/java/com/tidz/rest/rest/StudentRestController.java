05e84206b8f16fff92f13819e4bf81776ba8749a
Tiago Gomes <tidgomes@gmail.com>
2025-02-11 16:30:59 -0300

    create global exception handler

--- src/main/java/com/tidz/rest/rest/StudentRestController.java
+++ src/main/java/com/tidz/rest/rest/StudentRestController.java
@@ -35,23 +35,4 @@ public class StudentRestController {
         }
         return this.students.get(id);
     }
-
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) {
-        StudentErrorResponse error = new StudentErrorResponse();
-        error.setMessage(exc.getMessage());
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(Exception exc) {
-        StudentErrorResponse error = new StudentErrorResponse();
-        error.setMessage(exc.getMessage());
-        error.setStatus(HttpStatus.BAD_REQUEST.value());
-        error.setTimeStamp(System.currentTimeMillis());
-        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
-    }
 }
